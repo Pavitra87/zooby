0f24b5de1725d32b1fbbca2b6aa5df85620b2aeb
Pavitra <pavitramn2000@gmail.com>
2025-07-21 10:40:26 +0530

    covert json to js

---
+++ api/serviceKey.js
@@ -0,0 +1,40 @@
+module.exports = {
+  type: "service_account",
+  project_id: "zooby-app-6b2e6",
+  private_key_id: "bc1cae90647e67ae82862ca43ad6e7909eecc434",
+  private_key: `-----BEGIN PRIVATE KEY-----
+MIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQCa/urwjRhFzcSR
+dJgfAqQ+q13NlVNiuE7THuG3B8JCZdko1lDvrHUhwvN6z/VZhfRRc6FX3xmdaZcf
+9VmWIYtwNPspMyWaLJdDMWTIfMshc3biDeX3vle9Astbzj+WkW3iHiPuBc6NLqya
+XbzU02uCikoGPr7vctObHcA/wi350cTN0GvxKl7CvXeS9bptnfyD7ELV+UTkhDg5
+9qy4hhczGrir3ZIHipltbJqvmet77RLLfW+tvs4hzhn9/SVoXrO8YslsH8jgsrs2
+GGTqnsVt/9+pa/VtdBubOzieg0b/nNyTdLTqi4krGL1H/5M9be2BwiI0JMLAGTxc
+JB47X4FPAgMBAAECggEATDHcGjQqhgpBQ0Y/kCMSzhIdU3bAnsu/SE8qo/km+NAr
+wMUW1bQAgv/dBPddhExzTmVbICD3bShe2CGbkE/eReNtwxO3PQMFu0z5YAvlKgUi
+oL83WaKTbEIG6ZNLNEA69gleHWFyaHpU9K/3J+LlO6+y8z3+TCU1PFCUVWOmiWPe
+7nTVTjHGWriR8OUao1KHkhzb5zPg2H5m6QHJhU7ub4irJsao+TMijXmxgmgO4ZLJ
+iYwDMERYG6xUeTweTm/a1XJ1iyz+lbQerUS+X1aFId3LWeuwc2vizV96HMbvFGI8
+zerzhH4lSsIJZYPEEHwi0FpmHUGE7GAsJzD3sjrplQKBgQDYDOf+9SUFRFjCvzeQ
+PynH/8r837EW0MCNjRSF7DEG2wgucanJGevy8aEb9ZvFTlB4oiradAQynpcALZpa
+G+bobken6+D7tGRIGaVjZ5M7bay1/D4XbcS8+UkStnZHDPtlVoQdT+NIVgFrmqZS
+aomC1grAX8xam+9cQTerJXp2LQKBgQC3p+ROv+7DQUHs6wxOLm3DBLdvv+CiqGh0
+lnJChV74dm7oGxA9j4KwuqNGyTRwFjaiZhTLAnvyRU76NVO8dIS4WwEENP0P6/tl
+JqH2LSFAcTC4G09voMiLdTYymiTYy5wCZRzbGg/d4bf4StIuwbAFvKTByr/ewYJk
+vqZb3aLe6wKBgH03/b5nlu1Wj5tP+IRCF5UzTBMEEz+F/hQMkLiODFxM1pEUj7Ne
+ssI8CYSK9Z2Fpl5En1G9rDEqeZpA1BUl1s8gG6fXrq4z/2tKcfELNupaxp8zc0lg
+9YYRdajC2AqWG57AZhIXG0PxlSEooP1jgg+yvsiAvZBGkJSHuBwZvMCBAoGAHG9n
+PvYinqKk9WyzHAu+PNqnGCtKOYT/n0WH7Vq/CJM1usQt8IoevwQ2sYgHkW8vCFfr
+0j7ROAKZJs5I573WqsCUWLVnlSaBZvjjTL1SD6s20tqPowXu5wC/4nfRSqvDvO8w
+14+ic0U2ecAD6O9eNJUvoT2mFXQ6nySRl1mqlP8CgYEA0iqvnau3mrMEYvc7gABQ
++ByXCwXJjOcNP4ReVrj0bHH0yxAAFPNWQA437kzNH2cd8iW5Yjmc7ZpHhYJenCfx
+R4A1s5kI2/Izh+gO22Qm50LqlFA2HvnPAjXgcps+BFCNUojsXUH78f7gLQDMybqg
+hpaJQQVl0WWdJRrtDZwA1oU=
+-----END PRIVATE KEY-----`,
+  client_email: "firebase-adminsdk-fbsvc@zooby-app-6b2e6.iam.gserviceaccount.com",
+  client_id: "103862028900221699219",
+  auth_uri: "https://accounts.google.com/o/oauth2/auth",
+  token_uri: "https://oauth2.googleapis.com/token",
+  auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
+  client_x509_cert_url: "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-fbsvc%40zooby-app-6b2e6.iam.gserviceaccount.com",
+  universe_domain: "googleapis.com"
+};
